2b5a4780e381594f91f95cb4d3af96c17bb81833
wiktor-bandura <band.wiktor@gmail.com>
2022-11-25 09:19:56 +0100

    Created page for notes feauture

---
+++ page-my-notes.php
@@ -0,0 +1,36 @@
+<?php
+
+    if(!is_user_logged_in()) {
+        wp_redirect(esc_url(site_url('/')));
+        exit;
+    }
+    get_header();
+
+    while(have_posts()) {
+        the_post();
+        page_banner();
+        ?>
+        <div class="container container--narrow page-section">
+            <ul class="min-list link-list" id="my-notes">
+                <?php
+                    $user_notes = new WP_Query(array(
+                            'post_type' => 'note',
+                            'post_per_page' => -1,
+                            'author' => get_current_user_id()
+                    ));
+                    
+                    while($user_notes->have_posts()) {
+                        $user_notes->the_post(); ?>
+                            <li>
+                                <input value="<?php echo esc_attr(get_the_title()); ?>">
+                                <textarea><?php echo esc_attr(get_the_content()); ?></textarea>
+                            </li>    
+                   <?php }
+                ?>
+            </ul>
+        </div>
+        <?php
+    }
+    get_footer();
+
+?>
--- single-event.php
+++ single-event.php
@@ -10,10 +10,13 @@ while(have_posts()) {
 
 		<div class="metabox metabox--position-up metabox--with-home-link">
 			<p>
-				<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a>
+				<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    Events Home
+                </a>
 				<span class="metabox__main">
                         <?php the_title(); ?>
-                    </span>
+                </span>
 			</p>
 		</div>
 
